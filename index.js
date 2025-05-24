8916225d13c1f1572cb766ebe936cd5b17f628d4
BulkSkurk <adamtimra@hotmail.com>
2025-05-24 17:24:07 +0200

    MP-21 add WebApi fetch function

---
+++ index.js
@@ -0,0 +1,25 @@
+import { userToken } from './data/user';
+
+// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${userToken}`
+    },
+    method,
+    body: JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+async function getTopTracks() {
+  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+  return (await fetchWebApi('v1/me/top/tracks?time_range=long_term&limit=5', 'GET')).items;
+}
+
+const topTracks = await getTopTracks();
+console.log(
+  topTracks?.map(
+    ({ name, artists }) => `${name} by ${artists.map((artist) => artist.name).join(', ')}`
+  )
+);
