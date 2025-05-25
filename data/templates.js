afadeb5ddeb7ad479685b7735866c886176ada22
BulkSkurk <adamtimra@hotmail.com>
2025-05-25 18:59:45 +0200

    MP-24 Final commit, login depracted

--- data/templates.js
+++ data/templates.js
@@ -1,7 +1,7 @@
 export const templates = {
   favorites: `
-    <div class="playlists-navbar">
-        <p>Choose a playlist</p>
+    <div class="favorites-navbar">
+        <p>Choose an artist</p>
         <a href=# id="electric-bogaloo-list">Electric Bogaloo</a>
         <a href=# id="angeh-list">Angeh</a>
         <a href=# id="retro-modern-list">Retro-Modern</a>
@@ -11,3 +11,21 @@ export const templates = {
         <a href=# id="chill-hiphop-list">Chill HipHop</a>
     </div>`
 };
+export const playlist_id = {
+  electric_bogaloo: '5IKrzhvvStUh1SSdPzvyU2'
+};
+
+export function setPlayerTemplate(playlistID) {
+  return `
+        <iframe
+            title="Spotify Embed: Recommendation Playlist"
+            src="https://open.spotify.com/embed/playlist/${playlistID}?utm_source=generator"
+            width="100%"
+            height="100%"
+            frameborder="0"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          >
+        </iframe>    
+    `;
+}
--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import { userToken } from './data/user.js';
-import { templates } from './data/templates.js';
+import { templates, playlist_id, setPlayerTemplate } from './data/templates.js';
 
 setButtonListeners();
 
 function setButtonListeners() {
-  //   document.getElementById('playlists-btn').addEventListener('click', setPlaylistsTemplate());
-  document.querySelectorAll('#playlists-btn').forEach((button) => {
-    button.addEventListener('click', setPlaylistsTemplate);
-  });
+  document.getElementById('playlists-btn').addEventListener('click', setPlaylistsTemplate);
+  document
+    .getElementsByClassName('theme-btn')[0]
+    .addEventListener('click', setPlaylistsTemplate); /* Temporary, change later */
 }
 
-// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
 async function fetchWebApi(endpoint, method, body) {
   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
     headers: {
@@ -22,53 +21,13 @@ async function fetchWebApi(endpoint, method, body) {
   return await res.json();
 }
 
-async function getPlaylist(playlistId) {
-  let test = await fetchWebApi(`v1/playlists/${playlistId}`, 'GET').items;
-  console.log(test);
-}
-
 function addTemplateToAnyWindow(target, template) {
   document.getElementsByClassName(target)[0].innerHTML = template;
 }
-
 function setPlaylistsTemplate() {
-  console.log(templates.favorites);
   addTemplateToAnyWindow('spotify-window', templates.favorites);
-}
 
-function setPlayerTemplate(playlistId) {
-  return `
-        <iframe
-            title="Spotify Embed: Recommendation Playlist"
-            src="https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator"
-            width="100%"
-            height="100%"
-            frameborder="0"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="lazy"
-          >
-        </iframe>    
-    `;
-}
-
-function login() {
-  var redirect_uri = 'http://127.0.0.1:5500/callback';
-
-  var app = express();
-
-  app.get('/login', function (req, res) {
-    var state = generateRandomString(16);
-    var scope = 'user-read-private user-read-email';
-
-    res.redirect(
-      'https://accounts.spotify.com/authorize?' +
-        querystring.stringify({
-          response_type: 'code',
-          client_id: userToken.client_id,
-          scope: scope,
-          redirect_uri: redirect_uri,
-          state: state
-        })
-    );
+  document.getElementById('electric-bogaloo-list').addEventListener('click', () => {
+    addTemplateToAnyWindow('spotify-window', setPlayerTemplate(playlist_id.electric_bogaloo));
   });
 }
